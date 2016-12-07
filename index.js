3d1d377c5d2e27edf1dc810fc6a13b5c66c5be01
Al Ilseman <al@ilseman.com>
2016-12-07 17:19:19 -0600

    working uploads to "root"

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ var upload = multer({ dest: 'root/' });
 app.use('/files', serveIndex('root', {'icons': true}));
 app.use('/files', express.static('root'));
 
-app.post('/upload', upload.single('nameOfFile'), function (req, res) {
-    debugger;
+app.post('/upload', upload.single('lefile'), function (req, res) {
+    console.log(req.file);
+    res.send('im a response yo');
 });
 
 app.listen(3030, function () {
