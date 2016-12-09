c6746146e7b11b73979aef616bfd4594bfc14c4d
Al Ilseman <al@ilseman.com>
2016-12-09 14:31:18 -0600

    db-ish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     serveIndex = require('serve-index'),
-    multer = require('multer');
+    multer = require('multer'),
+    db = require('./db/config');
 
 var app = express();
 var upload = multer({ dest: 'root/' });
@@ -17,25 +18,10 @@ app.listen(3030, function () {
     console.log('Listening on localhost:3030');
 });
 
-
-//=======DATABASE=====
-
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database('db/file_repo')
-
-db.serialize(function () {
-    db.run('CREATE TABLE lorem (info TEXT)')
-    var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-
-    for (var i = 0; i < 10; i++) {
-        stmt.run('Ipsum ' + i)
-    }
-
-    stmt.finalize()
-
-    db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-        console.log(row.id + ': ' + row.info)
+db.any('select * from things')
+    .then(function (data) {
+        console.log(data);
     })
-})
-
-db.close()
+    .catch(function (error) {
+        console.log(error);
+    });
