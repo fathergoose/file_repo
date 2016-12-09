229926cbc2a62e5dc1561aa22fd69f23d3b80952
Al Ilseman <al@ilseman.com>
2016-12-09 14:55:31 -0600

    wires are connected

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+// Tutorial
+var db = require('../queries');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Tutorial
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
+
+module.exports = router;
+
