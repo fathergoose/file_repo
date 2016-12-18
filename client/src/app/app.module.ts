857c85c6981e2012727aa2c958e7e3d890ad8d88
Al Ilseman <al@ilseman.com>
2016-12-18 09:17:44 -0600

    a lil bit of routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,23 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FilesComponent } from './files/files.component';
 import { UploaderComponent } from './uploader/uploader.component';
 
+const appRoutes: Routes = [
+    { path: 'files', component: FilesComponent }
+    //{ path: '', component: AppComponent }
+]
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilesComponent,
-    UploaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FilesComponent,
+        UploaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
